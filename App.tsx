28bbd5090f280fb509efd35fb687af92e11c89fc
Olie Chan <olie.chan@infinityworks.com>
2020-01-14 18:03:13 +0000

    Styling changes

--- App.tsx
+++ App.tsx
@@ -12,14 +12,34 @@ import React, { useState } from 'react';
 import { Text, StyleSheet, View, SafeAreaView, Dimensions } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import Carousel, { Pagination } from 'react-native-snap-carousel';
+import { scaleSize } from './theme/mixins';
+import {
+  FONT_SIZE_16,
+  FONT_FAMILY_REGULAR,
+  FONT_FAMILY_BOLD,
+  FONT_SIZE_24,
+} from './theme/typography';
 
 declare var global: { HermesInternal: null | {} };
 const { width: screenWidth } = Dimensions.get('window');
+function WelcomePage() {
+  return (
+    <View style={[styles.container, styles.welcomeContainer]}>
+      <Text style={[styles.textBase, styles.textHeader]}>Welcome to Echo</Text>
+      <Text style={[styles.textBase]}>
+        We're the pharmacy that comes to you. Use our service to manage your
+        repeate NHS prescriptions, and get your medication delivered for free.
+      </Text>
+    </View>
+  );
+}
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <LinearGradient colors={['#085061', '#092853']} style={styles.container}>
-        <MyCarousel entries={['One', 'Two', 'Three']} />
+        <MyCarousel
+          entries={[<WelcomePage />, <Text>Two</Text>, <Text>Three</Text>]}
+        />
       </LinearGradient>
     </SafeAreaView>
   );
@@ -31,8 +51,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
+  welcomeContainer: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+  },
+  textBase: {
     color: 'white',
+    fontFamily: FONT_FAMILY_REGULAR,
+    fontSize: FONT_SIZE_16,
+  },
+  textHeader: {
+    fontFamily: FONT_FAMILY_BOLD,
+    fontSize: FONT_SIZE_24,
+  },
+  dots: {
+    width: scaleSize(10),
+    height: scaleSize(10),
+    borderRadius: scaleSize(5),
+    backgroundColor: 'white',
   },
 });
 export default App;
@@ -41,21 +77,14 @@ function MyCarousel({ entries }: any) {
   const [activeSlide, setActiveSlide] = useState(0);
 
   const _renderItem = ({ item }) => (
-    <View style={styles.container}>
-      <Text style={styles.text}>{item}</Text>
-    </View>
+    <View style={styles.container}>{item}</View>
   );
 
   const Pages = () => (
     <Pagination
       dotsLength={entries.length}
       activeDotIndex={activeSlide}
-      dotStyle={{
-        width: 10,
-        height: 10,
-        borderRadius: 5,
-        backgroundColor: 'white',
-      }}
+      dotStyle={styles.dots}
     />
   );
 
@@ -63,7 +92,7 @@ function MyCarousel({ entries }: any) {
     <View style={styles.container}>
       <Carousel
         sliderWidth={screenWidth}
-        itemWidth={screenWidth}
+        itemWidth={screenWidth * 0.8}
         data={entries}
         renderItem={_renderItem}
         onSnapToItem={setActiveSlide}
--- theme/typography.ts
+++ theme/typography.ts
@@ -1,4 +1,4 @@
-import {scaleFont} from './mixins';
+import { scaleFont } from './mixins';
 
 // FONT FAMILY
 export const FONT_FAMILY_REGULAR = 'OpenSans-Regular';
@@ -9,6 +9,7 @@ export const FONT_WEIGHT_REGULAR = '400';
 export const FONT_WEIGHT_BOLD = '700';
 
 // FONT SIZE
+export const FONT_SIZE_24 = scaleFont(24);
 export const FONT_SIZE_16 = scaleFont(16);
 export const FONT_SIZE_14 = scaleFont(14);
 export const FONT_SIZE_12 = scaleFont(12);
